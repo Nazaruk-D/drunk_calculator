3a52e45b5f24d19cad25a608a90c3e920caaf135
Nazaruk-D <nazaruk-dima@mail.ru>
2025-09-21 23:40:03 +0300

    fix gap

--- src/pages/Desktop/Calculator/Calculator.view.tsx
+++ src/pages/Desktop/Calculator/Calculator.view.tsx
@@ -8,19 +8,19 @@ export const Calculator = ({onCloseCalc}: Props) => {
     const {sign, input, input2, choseNumber, result, onSetResultHandler, setSignHandler, setValueHandler, onClearHandler} = useCalcActions()
     // const sign = input2 < 0 ? '-' : '+'
     return (
-      <div className="bg-white/20 backdrop-blur-lg rounded-3xl shadow-2xl p-6 border border-white/30 relative w-[300px] h-[500px]">
+      <div className="bg-white/20 backdrop-blur-lg rounded-3xl shadow-2xl p-6 border border-white/30 relative w-[300px] h-[500px] flex flex-col gap-2">
         <button onClick={onCloseCalc} className='absolute top-0 left-[-40px] hover:bg-gray-600/20 transition-colors duration-200 w-[36px] h-[36px] cursor-pointer rounded-full'  >
           <span className='text-amber-50 flex justify-center items-center'>x</span>
         </button>
 
-        <div className="bg-black/20 rounded-xl p-4 mb-4 text-right space-y-2">
+        <div className="bg-black/30 rounded-xl p-4 text-right space-y-2">
           <div className="text-white/70 text-sm min-h-[20px]">{input} {sign} {input2}</div>
           <div className="text-white text-3xl font-semibold truncate">{result}</div>
         </div>
 
         <div>
-        <div className="mb-6">
-          <div className="flex items-center justify-between bg-black/30 rounded-xl p-3 border border-white/20">
+        <div className="flex">
+          <div className="flex items-center justify-between bg-black/20 rounded-xl p-3 border border-white/20">
             <button 
               onMouseDown={() => setValueHandler("-")}
               className="w-10 h-10 cursor-pointer bg-red-500/80 text-white rounded-full flex items-center justify-center hover:bg-red-600 transition-colors text-lg font-bold active:scale-95"
@@ -52,11 +52,11 @@ export const Calculator = ({onCloseCalc}: Props) => {
         </div>
         </div>
         
-        <button onClick={onSetResultHandler} className="w-full text-white bg-black/20 hover:bg-black/30 rounded-xl p-4 mb-4 cursor-pointer">Установить значение</button>
-        <button onClick={onClearHandler} className="w-full text-white bg-black/20 hover:bg-black/30 rounded-xl p-4 mb-4 cursor-pointer">Сбросить значения</button>
+        <button onClick={onSetResultHandler} className="w-full text-white bg-black/20 hover:bg-black/30 rounded-xl p-4 cursor-pointer">Установить значение</button>
+        <button onClick={onClearHandler} className="w-full text-white bg-black/20 hover:bg-black/30 rounded-xl p-4 cursor-pointer">Сбросить значения</button>
       
             {/* Ряд кнопок с операциями + - / * */}
-        <div className="grid grid-cols-4 gap-2 mb-4">
+        <div className="grid grid-cols-4 gap-2">
           <button 
             onClick={() => setSignHandler("+")}
             className="bg-black/20 hover:bg-black/30 text-white p-3 rounded-lg font-bold transition-colors active:scale-95 cursor-pointer"
